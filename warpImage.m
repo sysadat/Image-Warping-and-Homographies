fad42fdb0b1c5009b0334b50531ead661e600837
Sayed Yoseph Sadat <42789651+sysadat@users.noreply.github.com>
2020-05-14 04:14:44 -0700

    Add files via upload

---
+++ warpImage.m
@@ -0,0 +1,10 @@
+function [warpIm, mergeIm] = warpImage(inputIm, refIm, H)
+    
+    inputIm = double(inputIm);
+    refIm = double(refIm);
+    
+    [inputHeight, inputWidth, inputDimension] = size(inputIm);
+    [refHeight, refWidth, refDimension] = size(refIm);
+
+end
+
